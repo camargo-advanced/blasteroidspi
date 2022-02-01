abbb294a34e5cf0cf952b8b6bf9be2c98a09b393
Marcelo Camargo <camargo.advanced@gmail.com>
2022-02-01 09:45:34 -0300

    attribute order assignment in entity constructor

--- entity.py
+++ entity.py
@@ -6,8 +6,8 @@ class Entity():
     def __init__(self, galaxy, name, color):
         self.galaxy = galaxy
         self.name = name
-        self.position = Vector2(0.0, 0.0)
         self.color = color
+        self.position = Vector2(0.0, 0.0)
         self.dead = False
 
     def update(self, time_passed, event_list):
