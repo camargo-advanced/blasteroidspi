4ea2d83fea7a17f1a21af0331966d4ad95e3246c
Marcelo <camargo.advanced@gmail.com>
2022-02-05 19:29:03 -0300

    video class 8

--- galaxy.py
+++ galaxy.py
@@ -17,8 +17,35 @@ class Galaxy():
         time_passed_seconds = time_passed / 1000.0
         for entity in self.entities.values():
             entity.update(time_passed_seconds, event_list)
+            if not self.in_screen_space(entity.position):
+                # entities require authorization to leave the galaxy,
+                # thus we must keep entities inside it !
+                self.wrap_coordinates(entity.position)
 
     def render(self, surface):
         surface.fill(BLACK)
         for entity in self.entities.values():
             entity.render(surface)
+
+    def wrap_coordinates(self, position):
+        width, height = self.rect.width, self.rect.height
+        if position.x < 0.0:
+            position.x += width
+        if position.x >= width:
+            position.x -= width
+        if position.y < 0.0:
+            position.y += height
+        if position.y >= height:
+            position.y -= height
+
+    def in_screen_space(self, position):
+        width, height = self.rect.width, self.rect.height
+        if (position.x < 0.0):
+            return False
+        if (position.x >= width):
+            return False
+        if (position.y < 0.0):
+            return False
+        if (position.y >= height):
+            return False
+        return True
