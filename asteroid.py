88fc2fde4f0eacf7a0493c36abd543eb8cbd96a8
Marcelo <camargo.advanced@gmail.com>
2022-02-26 14:54:31 -0300

    video class 16

--- asteroid.py
+++ asteroid.py
@@ -8,7 +8,6 @@ WIDTH = 3
 SCALE_FACTOR = 3
 SPEED = 90.0  # pixels per second
 ANGULAR_SPEED = 21.0  # degrees per second
-
 ASTEROID_WIREFRAME = [
     Vector2(-20.0, 20.0), Vector2(-25.0, 5.0), Vector2(-25.0, -10.0),
     Vector2(-5.0, -10.0), Vector2(-10.0, -20.0), Vector2(5.0, -20.0),
@@ -32,9 +31,39 @@ class Asteroid(WEntity):
         self.angular_speed = ANGULAR_SPEED
         self.rotating = CLOCKWISE
         self.size = SCALE_FACTOR
+#>>>>>
+        self.times_hit = 0
+        self.exploding = False
 
     def update(self, time_passed, event_list):
         super().update(time_passed, event_list)
 
+#>>>>>
+        for entity in self.galaxy.get_entities_by_name('blast'):
+            if self.collide(entity):
+                # if a blast hit me, I need to break myself
+                # into 2 smaller rocks !
+                self.exploding = True
+                self.times_hit += 1
+                if self.times_hit < 3:
+                    self.size /= 2
+                    self.velocity *= 1.5
+                    self.velocity.rotate_ip(random()*360)
+                    self.galaxy.add_entity(self.fragment())
+                    entity.dead = True
+                else:
+                    self.exploding = True
+                    self.dead = True
+
     def render(self, surface):
         super().render(surface)
+
+#>>>>>
+    def fragment(self):
+        fragment = Asteroid(self.galaxy)
+        fragment.position = Vector2(self.position)
+        fragment.velocity = Vector2(self.velocity)
+        fragment.velocity.rotate_ip(random()*360)
+        fragment.size = self.size
+        fragment.times_hit = self.times_hit
+        return fragment
