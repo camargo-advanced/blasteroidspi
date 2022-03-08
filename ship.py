6d2ca7c2a2cf7b92c70b258bc23605366cadf514
Marcelo <camargo.advanced@gmail.com>
2022-03-08 19:50:57 -0300

    video class 25

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 from pygame.locals import *
-from blast import Blast 
+from blast import Blast
+from sound import Sound 
 from wentity import WEntity
 from pygame.math import Vector2
 from utils import *
@@ -31,6 +32,8 @@ class Ship(WEntity):
         self.size = SCALE_FACTOR
         self.shielded = True
         self.firing = False 
+#>>>>>
+        self.dying = False
 
     def update(self, time_passed, event_list):
         super().update(time_passed, event_list)
@@ -46,13 +49,14 @@ class Ship(WEntity):
             # and then add it to the galaxy
             blast = Blast(self.galaxy, Vector2(self.position), self.angle)
             self.galaxy.add_entity(blast)
-            self.firing = False
 
         for entity in self.galaxy.get_entities_by_name('asteroid'):
             if not self.shielded and self.collide(entity):
                 # if a rock hit me, I lose a life but I'm shielded for 5 sec!
                 # I also need to be positioned at the center of screen stationary,
                 # and in the same angle I was born. The lives must be reduced by 1               
+#>>>>>
+                self.dying = True
                 self.shield()
                 pygame.time.set_timer(UNSHIELD_EVENT, 2500, 1)
                 self.position = Vector2(self.galaxy.rect.width/2,
@@ -65,9 +69,19 @@ class Ship(WEntity):
         super().render(surface)
 
         if self.accelerating == FORWARD:
+#>>>>>
+            Sound().play('thrust')
             self.wireframe = THRUST_WIREFRAME
             super().render(surface)
             self.wireframe = SHIP_WIREFRAME
+#>>>>>
+        if self.firing:
+            Sound().play('fire')
+            self.firing = False
+#>>>>>
+        if self.dying:
+            Sound().play('bang')
+            self.dying = False
 
     def process_events(self, event_list):
         for event in event_list:
---
+++ sound.py
@@ -0,0 +1,54 @@
+import os
+import pygame
+
+
+class Sound():
+    """Load game sounds and make them availaboe to play.
+    This class follows the singleton design pattern. """
+    _instance = None
+    _sounds = {}
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Sound, cls).__new__(cls)
+            cls._sounds = {
+                'bang': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'bang.wav')),
+                    'channel': pygame.mixer.Channel(0),
+                    'overlap': True
+                },
+                'beep': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'beep.wav')),
+                    'channel': pygame.mixer.Channel(2),
+                    'overlap': True
+                },
+                'fire': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'fire.wav')),
+                    'channel': pygame.mixer.Channel(2),
+                    'overlap': True
+                },
+                'siren': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'siren.wav')),
+                    'channel': pygame.mixer.Channel(2),
+                    'overlap': True
+                },
+                'thrust': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'thrust.wav')),
+                    'channel': pygame.mixer.Channel(1),
+                    'overlap': False
+                },
+                'beep-countdown': {
+                    'sound_key': pygame.mixer.Sound(os.path.join('res', 'beep-countdown.wav')),
+                    'channel': pygame.mixer.Channel(2),
+                    'overlap': True
+                }
+            }
+        return cls._instance
+
+    def play(self, sound_key):
+        sound = self._sounds[sound_key]['sound_key']
+        channel = self._sounds[sound_key]['channel']
+        overlap = self._sounds[sound_key]['overlap']
+        if overlap or not overlap and not channel.get_busy():
+            channel.play(sound)
+        
